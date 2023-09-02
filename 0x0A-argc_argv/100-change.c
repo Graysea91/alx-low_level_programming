3f1cd6e11ef24c2ada29b4ad178b6c734f630b74
Graysea91 <kennykidsjpjb@gmail.com>
2023-09-01 18:54:22 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
+
 /**
-* main - add all argument  it receives
-* @argc: number of arguments
-* @argv: array of arguments
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int i;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc == 1)
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	for (i = 1; i < argc; i++)
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		int j;
-
-		for (j = 0; argv[i][j] != '\0'; j++)
+		while (num >= coins[j])
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			result++;
+			num -= coins[j];
 		}
-
-		sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+
+	printf("%d\n", result);
 	return (0);
 }
