f7dfd37738d1df56dbe7b49a7e9188a6ecfc5b4e
Graysea91 <kennykidsjpjb@gmail.com>
2023-09-11 18:58:31 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - short description
-* @name: name member
-* @age: age member
-* @owner: owner member
+* struct dog - defined data type
+* @name: dog name
+* @age: dog age
+* @owner: owner of dog
 */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct dog my_dog;
+/**
+* dog_t - new type for struct dog
+*/
+typedef  struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
